d4a9ca80fbc7415244ce1d135d1454c2ec33fb59
keenanstiles <159201743+keenanstiles@users.noreply.github.com>
2024-02-09 14:58:53 +1300

    Add files via upload
    new file

---
+++ keengan.py
@@ -0,0 +1,104 @@
+from tkinter import *
+
+
+class Converter:
+
+    def __init__(self):
+
+        # common format for all buttons
+
+        button_font = ("Arial", "14", "bold")
+        button_fg = "#FFFFFF"
+
+        # Set up GUI Frame
+        self.temp_frame = Frame()
+        self.temp_frame.grid()
+
+        self.temp_heading = Label(self.temp_frame,
+                                  text="Temperature Convertor",
+                                  font=("Arial", "16", "bold")
+                                  )
+        self.temp_heading.grid(row=0)
+
+        instructions = "Please enter a temperature below and" \
+                       "then press one of the buttons to convert" \
+                       "it from centigrade to Fahrenheit."
+        self.temp_instructions = Label(self.temp_frame,
+                                       text=instructions,
+                                       wrap=250, width=40,
+                                       justify="left")
+        self.temp_instructions.grid(row=1)
+
+        self.temp_entry = Entry(self.temp_frame,
+                                font=("Arial", "14")
+                                )
+        self.temp_entry.grid(row=2, padx=10, pady=10)
+
+        error = "Please enter a number"
+        self.temp_error = Label(self.temp_frame, text=error,
+                                fg="#9C0000")
+        self.temp_error.grid(row=3)
+
+        # Conversion, help and history / export buttons
+        self.button_frame = Frame(self.temp_frame)
+        self.button_frame.grid(row=4)
+
+        self.to_celsius_button = Button(self.button_frame,
+                                        text="To Celsius",
+                                        bg="#990099",
+                                        fg=button_fg,
+                                        font=button_font, width=12,
+                                        command=self.to_celsius)
+        self.to_celsius_button.grid(row=0, column=0, padx=5, pady=5)
+
+        self.to_fahrenheit_button = Button(self.button_frame,
+                                        text="To Fahrenheit",
+                                        bg="#009900",
+                                        fg=button_fg,
+                                        font=button_font, width=12)
+        self.to_fahrenheit_button.grid(row=0, column=1, padx=5, pady=5)
+
+        self.to_help_button = Button(self.button_frame,
+                                        text="Help / Info",
+                                        bg="#CC6600",
+                                        fg=button_fg,
+                                        font=button_font, width=12)
+        self.to_help_button.grid(row=1, column=0, padx=5, pady=5)
+
+        self.to_history_button = Button(self.button_frame,
+                                           text="History / Export",
+                                           bg="#004C99",
+                                           fg=button_fg,
+                                           font=button_font, width=12,
+                                        state=DISABLED)
+        self.to_history_button.grid(row=1, column=1, padx=5, pady=5)
+
+
+    def check_temp(min_value):
+        error = ("please enter a number that is more " \
+                 "than {}".format(min_value))
+
+        try:
+            response = self.temp_entry.get
+
+            if response < min_value:
+                print(error)
+            else:
+                return response
+
+        except ValueError:
+            print(error)
+
+    # check temperature is more than -459 before converting it
+    def to_celsius(self):
+
+        self.check_temp(-459)
+
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperture Convertor")
+    Converter()
+    root.mainloop()
