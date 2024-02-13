5a8310c46df24f897ad50f7e10366fcaedbfa881
keenanstiles <159201743+keenanstiles@users.noreply.github.com>
2024-02-13 14:59:25 +1300

    p5 Tuesday

---
+++ test v5 (2).py
@@ -0,0 +1,189 @@
+from tkinter import *
+
+
+class Converter:
+
+    def __init__(self):
+
+        #initialise variables (such as the feedback variables)
+        self.var_feedback = StringVar()
+        self.var_feedback.set("no")
+
+        self.var_has_error = StringVar()
+        self.var_has_error.set("no")
+
+        # common format for all buttons
+
+        button_font = ("Arial", "14", "bold")
+        button_fg = "#FFFFFF"
+
+        # Set up GUI Frame
+        self.temp_frame = Frame()
+        self.temp_frame.grid()
+
+        self.temp_heading = Label(self.temp_frame,
+                                  text="Temperature Convertor",
+                                  font=("Arial", "16", "bold")
+                                  )
+        self.temp_heading.grid(row=0)
+
+        instructions = "Please enter a temperature below and" \
+                       "then press one of the buttons to convert" \
+                       "it from centigrade to Fahrenheit."
+        self.temp_instructions = Label(self.temp_frame,
+                                       text=instructions,
+                                       wrap=250, width=40,
+                                       justify="left")
+        self.temp_instructions.grid(row=1)
+
+        self.temp_entry = Entry(self.temp_frame,
+                                font=("Arial", "14")
+                                )
+        self.temp_entry.grid(row=2, padx=10, pady=10)
+
+        error = "Please enter a number"
+        self.output_label = Label(self.temp_frame, text="",
+                                  fg="#9C0000")
+        self.output_label.grid(row=3)
+
+        # Conversion, help and history / export buttons
+        self.button_frame = Frame(self.temp_frame)
+        self.button_frame.grid(row=4)
+
+        self.to_celsius_button = Button(self.button_frame,
+                                        text="To Celsius",
+                                        bg="#990099",
+                                        fg=button_fg,
+                                        font=button_font, width=12,
+                                        command=lambda: self.temp_convert(-459))
+        self.to_celsius_button.grid(row=0, column=0, padx=5, pady=5)
+
+        self.to_fahrenheit_button = Button(self.button_frame,
+                                        text="To Fahrenheit",
+                                        bg="#009900",
+                                        fg=button_fg,
+                                        font=button_font, width=12,
+                                        command=lambda: self.temp_convert(-273))
+        self.to_fahrenheit_button.grid(row=0, column=1, padx=5, pady=5)
+
+        self.to_help_button = Button(self.button_frame,
+                                        text="Help / Info",
+                                        bg="#CC6600",
+                                        fg=button_fg,
+                                        font=button_font, width=12)
+        self.to_help_button.grid(row=1, column=0, padx=5, pady=5)
+
+        self.to_history_button = Button(self.button_frame,
+                                           text="History / Export",
+                                           bg="#004C99",
+                                           fg=button_fg,
+                                           font=button_font, width=12,
+                                        state=DISABLED)
+        self.to_history_button.grid(row=1, column=1, padx=5, pady=5)
+
+  #just giving it a little peek to be sure its valid prior to conversion
+
+    def check_temp(self, min_value):
+
+        has_error = "no"
+        error = ("ENTER AN INTEGER GREATER THAN {}".format(min_value))
+        # check the number is valid
+
+        response = self.temp_entry.get()
+
+        try:
+
+            response = float(response)
+
+            if response < min_value:
+                has_error = "yes"
+
+        except ValueError:
+            has_error = "yes"
+
+        # sets var has error blah blah
+        # formatting stuff yedda yadda
+        if has_error == "yes":
+            self.var_has_error.set("yes")
+            self.var_feedback.set(error)
+            return "invalid"
+
+        # if no errors (woop woop!)
+        else:
+            self.var_has_error.set("no")
+
+            # return the number
+            # converted and enable history button
+
+            self.to_history_button.config(state=NORMAL)
+            return response
+
+    @staticmethod
+    def round_ans(val):
+        var_rounded = (val * 2 + 1) // 2
+        return "{:.0f}".format(var_rounded)
+
+
+    # check temperature is valid before converting it
+    def temp_convert(self, min_val):
+        to_convert = self.check_temp(min_val)
+        deg_sign = u'\N{DEGREE SIGN}'
+        set_feedback = "yes"
+        answer = ""
+        from_to = ""
+
+        if to_convert == "invalid":
+            set_feedback = "no"
+
+        # Convert to centigrade
+        elif min_val == -459:
+            # do calculation
+            answer = (to_convert - 32) * 5 / 9
+            from_to = "{} F{} converts to {} C{}"
+
+        # convert to fahrenheit
+        else:
+            answer = to_convert * 1.8 + 32
+            from_to = "{} C{} converts to {} F{}"
+
+        if set_feedback == "yes":
+            to_convert = self.round_ans(to_convert)
+            answer = self.round_ans(answer)
+
+            feedback = from_to.format(to_convert, deg_sign,
+                                      answer, deg_sign)
+            self.var_feedback.set(feedback)
+
+        self.output_answer()
+
+
+
+
+
+    # shows user output and clears entry widget
+    # ready for next calculation
+
+    def output_answer(self):
+        output = self.var_feedback.get()
+        has_errors = self.var_has_error.get()
+
+        if has_errors == "yes":
+            self.output_label.config(fg="#9C0000")
+            self.temp_entry.config(bg="#F8CECC")
+
+        else:
+            self.output_label.config(fg="#004C00")
+            self.temp_entry.config(bg="#FFFFFF")
+
+
+        self.output_label.config(text=output)
+
+
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperture Convertor")
+    Converter()
+    root.mainloop()
