bb2f27cfc142f23df0034120f82c9fdb61a535e6
keenanstiles <159201743+keenanstiles@users.noreply.github.com>
2024-02-17 13:32:16 +1300

    too damn hard, sat sesh, midish 13

--- 07_history_gooey.py
+++ 07_history_gooey.py
@@ -10,6 +10,14 @@ class Converter:
         button_font = ("Arial", "12", "bold")
         button_fg = "#ffffff"
 
+        # Five item list
+        # self.all_calculations = ['0 F
+        #
+        #
+
+        # six itemlist
+        self.all_calculations = ['']
+
         # gui frame
         self.temp_frame = Frame(padx=10, pady=10)
         self.temp_frame.grid(row=0)
@@ -17,41 +25,41 @@ class Converter:
         self.button_frame = Frame(padx=30, pady=30)
         self.button_frame.grid(row=0)
 
-        self.to_help_button = Button(self.button_frame,
-                                     text="Help / Info",
+        self.to_history_button = Button(self.button_frame,
+                                     text="Conversion history",
                                      bg="#CC6600",
                                      fg=button_fg,
                                      font=button_font, width=12,
-                                     command=self.to_help)
-        self.to_help_button.grid(row=1, column=0, padx=5, pady=5)
+                                     command=self.to_history)
+        self.to_history_button.grid(row=1, column=0, padx=5, pady=5)
 
-    def to_help(self):
-        DisplayHelp(self)
+    def to_history(self):
+        HistoryExport(self)
 
 
-class DisplayHelp:
+class HistoryExport:
 
     def __init__(self, partner):
         background = "#ffe6cc"
-        self.help_box = Toplevel()
+        self.history_box = Toplevel()
 
-        partner.to_help_button.config(state=DISABLED)
+        partner.to_history_button.config(state=DISABLED)
 
         # yadda yadda more stuf to make help button active again if no
         # window present
-        self.help_box.protocol('WM_DELETE_WINDOW',
-                               partial(self.close_help,partner))
+        self.history_box.protocol('WM_DELETE_WINDOW',
+                               partial(self.close_history,partner))
 
-        self.help_frame = Frame(self.help_box, width=300, height=200,
+        self.history_frame = Frame(self.history_box, width=300, height=200,
                                 bg=background)
-        self.help_frame.grid()
+        self.history_frame.grid()
 
-        self.help_heading_label = Label(self.help_frame, bg=background,
+        self.history_heading_label = Label(self.history_frame, bg=background,
                                         text="help / info",
                                         font=("Arial", "14", "bold"))
-        self.help_heading_label.grid(row=0)
+        self.history_heading_label.grid(row=0)
 
-        help_text = "To use the program, simply enter the temperature " \
+        history_text = "To use the program, simply enter the temperature " \
                     "you wish to convert and then choose to convert " \
                     "to either degrees celsius or " \
                     "fahrenheit.. \n\n" \
@@ -59,24 +67,24 @@ class DisplayHelp:
                     "(-459 F) is absolute zero (the coldest possible " \
                     "temperature), try no convert less then this and youll get an error message \n\n " \
                     "go to the history tab to download recent calculations into a text file"
-        self.help_text_label = Label(self.help_frame, bg=background,
-                                     text=help_text, wraplength=350,
+        self.history_text_label = Label(self.history_frame, bg=background,
+                                     text=history_text, wraplength=350,
                                      justify="left")
-        self.help_text_label.grid(row=1, padx=10)
+        self.history_text_label.grid(row=1, padx=10)
 
-        self.dismiss_button = Button(self.help_frame,
+        self.dismiss_button = Button(self.history_frame,
                                      font=("Arial", "12", "bold"),
                                      text="dismiss", bg="#CC6600",
                                      fg="#ffffff",
-                                     command=partial(self.close_help,
+                                     command=partial(self.close_history,
                                                      partner))
         self.dismiss_button.grid(row=2, padx=10, pady=10)
 
     # closes help dialouge
-    def close_help(self, partner):
+    def close_history(self, partner):
         # put help button back to normal
-        partner.to_help_button.config(state=NORMAL)
-        self.help_box.destroy()
+        partner.to_history_button.config(state=NORMAL)
+        self.history_box.destroy()
 
 
 if __name__ == "__main__":
